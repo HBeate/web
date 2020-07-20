9d9f5e85f393fcca6e70bc8f05ea539f16665968
HBeate <hauser_b@hotmail.com>
2020-07-20 20:15:32 +0200

    JS calculator done

--- Aufgabe3/index.js
+++ Aufgabe3/index.js
@@ -1,39 +1,53 @@
-const names = [
-   {usrname: "Lara", pw: 3101}, 
-   {usrname: "Marvin", pw: 2811}, 
-   {usrname: "Alina", pw: 2405}, 
-   {usrname: "Lukas", pw: 2404}
+
+
+/***********************************
+ * PASSWORTSCHUTZ
+ ***********************************/
+
+
+/*const names = [
+    { usrname: "Lara", pw: 3101 },
+    { usrname: "Marvin", pw: 2811 },
+    { usrname: "Alina", pw: 2405 },
+    { usrname: "Lukas", pw: 2404 }
 ]
 
-
-
-function clickLogin(){
+function clickLogin() {
 
     var name = document.getElementById("usrname").value;
     var password = document.getElementById("pw").value;
     var hasFound = false;
-    for(let i = 0; i < names.length; i ++){
+    for (let i = 0; i < names.length; i++) {
         if (name === names[i].usrname && password == names[i].pw) {
             hasFound = true;
             break;
         }
     }
-
-    if(hasFound){
+    if (hasFound) {
         window.location.href = "https://www.kooiker-fr.com";
     } else {
-        alert("falsch");
+        alert("Input incorrect");
     }
-    /*
-    names.forEach(checkInput);
-    function checkInput(name) {
-        var name = document.getElementById("usrname");
-        var password = document.getElementById("pw");
-        if (name == usrname & password == pw) {
-            window.location.href = "https://www.kooiker-fr.com";
-        } else {
-            alert("falsch");
-        }
-    }*/
-
+}*/
+/***********************************
+ * CALCULATOR
+ ***********************************/
+var inputString = "";
+function addCalculation(id) {
+        var input = document.getElementById(id).value;
+        inputString += input;
+        console.log(inputString);
+        document.getElementById('calculation').value=inputString;
+}
+function solveCalculation() {
+       var result= "";
+       result= eval(inputString);
+       document.getElementById('result').value=result;
+       inputString=result;
+       result = ""
+}
+function clearAll(){
+        document.getElementById('calculation').value="";
+        document.getElementById('result').value="";
+        inputString="";
 }
