c9cc0639ff8cdbf737480042b780c1d5d9de6076
HBeate <hauser_b@hotmail.com>
2020-08-04 11:31:25 +0200

    individual *.js for each task

---
+++ Aufgabe3/passwortschutz.js
@@ -0,0 +1,29 @@
+
+
+/***********************************
+ * PASSWORTSCHUTZ
+ ***********************************/
+const names = [
+    { usrname: "Lara", pw: 3101 },
+    { usrname: "Marvin", pw: 2811 },
+    { usrname: "Alina", pw: 2405 },
+    { usrname: "Lukas", pw: 2404 }
+]
+function clickLogin() {
+
+    var name = document.getElementById("usrname").value;
+    var password = document.getElementById("pw").value;
+    var hasFound = false;
+    for (let i = 0; i < names.length; i++) {
+        if (name === names[i].usrname && password == names[i].pw) {
+            hasFound = true;
+            break;
+        }
+    }
+
+    if (hasFound) {
+        window.location.href = "https://www.kooiker-fr.com";
+    } else {
+        alert("Input incorrect");
+    }
+}
