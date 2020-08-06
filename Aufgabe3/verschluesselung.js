5b7cc7cad262496e9df2c568d902e2c744fe2beb
HBeate <hauser_b@hotmail.com>
2020-08-06 12:57:26 +0200

    working on decode & delete

--- Aufgabe3/verschluesselung.js
+++ Aufgabe3/verschluesselung.js
@@ -2,48 +2,41 @@
 /***********************************
  * ONLINE VERSCHLÜSSELUNGSTOOL
 ***********************************/
-var originalMessage = "";
-var encryptedMessage = "";
+//var originalMessage = "";
+//var encryptedMessage = "";
 var myMessage = "";
+var isDecode = true;
 messageArray = [];
 
-function selectOffset() {
+// you'll do it!!!!
+function codeMessage(newLetter, isDecode) {
+    this.isDecode = isDecode;
+    var decoded = document.getElementById(this.isDecode ? 'decoded' : 'coded').value;
     var offset = document.getElementById('offset').value;
-    console.log(offset);
-}
-function codeMessage(newLetter) {
-    var decoded = document.getElementById('decoded').value;
-    var offset = document.getElementById('offset').value;
-    console.log(offset);
-    //    var coded = document.getElementById('coded').value;
-    //    console.log(decoded);
+
     for (let i = 0; i < decoded.length; i++) {
         var myChar = decoded.charAt(i);
-        // TODO check here for Umlaute with Switch
         myMessage = myMessage + myChar;
     }
     console.log("message " + myMessage);
-    if(newLetter)
+    if (newLetter)
         messageArray.push(myChar);  // here the individual character is an item in the array !!! TODO add delete option !!!
-    else
-        {
-            messageArray = [];
-            for(let i = 0; i < decoded.length; i++){
-                messageArray.push(decoded.charAt(i));
-                encrypt(offset);
-            }
+    else {
+        messageArray = [];
+        for (let i = 0; i < decoded.length; i++) {
+            messageArray.push(decoded.charAt(i));
+            encrypt(offset);
         }
-    //   messageArray = [];
-    //   messageArray.push(myMessage);   // here the string is one singel entry in the array
+    }
+
     myMessage = "";
     myChar = "";
     console.log("Array " + messageArray);
-    // TODO convert to upper case????
-    if(newLetter)
-        document.getElementById('coded').value = decoded;
-    //  originalMessage.value = encryptedMessage.value ;
 
-    if(newLetter)
+    if (newLetter)
+        document.getElementById(this.isDecode ? 'coded' : 'decoded').value = decoded;
+
+    if (newLetter)
         encrypt(offset);
 }
 function encrypt(offset) {
@@ -51,8 +44,6 @@ function encrypt(offset) {
     var myNewMessage = "";
     var decrypt = false;
     var toEncrypt = messageArray[messageArray.length - 1];
-    //     for (let i = 0; i < messageArray.length; i++) {
-    //     var myChar = messageArray.charAt(i);
     switch (toEncrypt) {
         case '!':
         case '?':
@@ -70,14 +61,16 @@ function encrypt(offset) {
             break;
         default:
             if (decrypt) {
-                messageArray[messageArray.length - 1] -= offset;
-            } else {                         // rutscht im ASCII code um zwei Stellen weiter
+                messageArray[messageArray.length - 1] = String.fromCharCode(toEncrypt.charCodeAt(0) - offset);
+            } else {                         
                 messageArray[messageArray.length - 1] = String.fromCharCode(toEncrypt.charCodeAt(0) + offset);
             }
             for (let i = 0; i < messageArray.length; i++) {
                 myNewMessage = myNewMessage + messageArray[i];
             }
-        //  myNewMessage = myNewMessage + toEncrypt;
-    }    document.getElementById('coded').value = myNewMessage;
-    //}
+    }    if (decrypt) {
+        document.getElementById(this.isDecode ? 'decoded' : 'coded').value = myNewMessage;
+    } else {
+        document.getElementById(this.isDecode ?'coded' : 'decode').value = myNewMessage;
+    }
 }
