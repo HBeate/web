81060d36325c17cd57b4da79462468d06907ccd0
HBeate <hauser_b@hotmail.com>
2020-08-17 04:24:44 +0200

    checkbox bug bei deleteRow and then adding a task

--- Aufgabe3/todo.js
+++ Aufgabe3/todo.js
@@ -18,7 +18,7 @@ function addItemToArray(){
 
 
 let tasks = [];
-var isChecked = false;
+var isChecked = "";
 var rowLength = "";
 var id = 1;
 var myCheckbox = "";
@@ -38,19 +38,14 @@ const addTask = (event) => {
         id: id,
         name: document.getElementById("newToDo").id,
         toDo: document.getElementById("newToDo").value,
-        isChecked: false
+        isChecked: isChecked
     }
 
     id = id + 1;
     tasks.push(task)
     document.getElementById('newToDo').value = "";
 
-    //  for display purposes only
-    console.warn('added', { tasks });
-    let pre = document.querySelector('#msg pre');
-    pre.textContent = '\n' + JSON.stringify(tasks, '\t', 2);
-
-    for (let i = 0; i < tasks.length; i++) {
+    for (let i = 0; i < tasks.length; i++) { 
 
         var table = document.getElementById("myTable");
         var row = table.insertRow(i);
@@ -61,31 +56,38 @@ const addTask = (event) => {
         myCheckbox = document.createElement("INPUT");
         myCheckbox.setAttribute("type", "checkbox");
         myCheckbox.id = task.id;
+        myCheckbox.checked = tasks[i].isChecked;
 
-        myCheckbox.onclick = function () {
-            //        HIER BEKOMME ICH DIE FEHLERMELDUNG: Cannot read property 'checked' of undefined
-            var x = document.getElementById("myTable").myCheckbox.checked;
-            console.log("the checkbox " + tasks[i].id + " is " + tasks[i].isChecked)
-        }
-        console.log("Checkbox ID: " + myCheckbox.id);
         cell1.appendChild(myCheckbox);
         cell2.innerHTML = tasks[i].toDo;
         var xButton = document.createElement("button");
         xButton.innerHTML = "X";
+        cell3.appendChild(xButton);
+        numberToDos();
 
         xButton.onclick = function () {
             delete tasks[i];
             filterArray();
-            console.log("Array after deleting a row: " + tasks);
             deleteRow(this);
-
             numberToDos();
         };
-        cell3.appendChild(xButton);
-        numberToDos();
+        myCheckbox.onclick = function () {
+            var x = table.rows[i].cells[0].getElementsByTagName("input")[0].checked
+            tasks[i].isChecked = x;
+        }
     }
     storeTasks();
+        //  for display purposes only
+        console.warn('added', { tasks });
+        let pre = document.querySelector('#msg pre');
+        pre.textContent = '\n' + JSON.stringify(tasks, '\t', 2);
 }
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => { // calls the event when the button is clicked
     document.getElementById('submitBtn').addEventListener('click', addTask);
 });
@@ -94,7 +96,6 @@ input.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         document.getElementById("submitBtn").click();
-
     }
 });
 
@@ -102,18 +103,14 @@ function filterArray() {
     var filtered = tasks.filter(function (newToDo) {
         return newToDo != null;
     });
-
-    console.log(filtered);
     tasks = [];
     tasks = filtered;
-    console.log("Array after filterArray: " + tasks);
 }
 
 function clearTable() {
     for (let i = rowLength - 1; i >= 0; i--) {
         document.getElementById("myTable").deleteRow(i);
     }
-
 }
 
 function numberToDos() {
@@ -129,7 +126,6 @@ function deleteRow(row) {
         tasks = [];
         id = 1;
     }
-
 }
 
 function deleteAllRows() {
@@ -139,10 +135,12 @@ function deleteAllRows() {
         numberToDos();
         id = 1;
     }
-
 }
 
- function storeTasks() {
+function fillTable() {
+
+}
+function storeTasks() {
     localStorage.setItem("toDoList", JSON.stringify(tasks));
 
     /* var retrievedData = localStorage.getItem("toDoList");
