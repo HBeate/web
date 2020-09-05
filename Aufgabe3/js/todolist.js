9d482b7fa44cc718f25b81d9edaa01f785947df8
HBeate <hauser_b@hotmail.com>
2020-09-05 13:58:27 +0200

    needs formatting

--- Aufgabe3/js/todolist.js
+++ Aufgabe3/js/todolist.js
@@ -16,67 +16,69 @@ document.getElementById("addTask").addEventListener("click", function () {
 document.getElementById("txtResponsible").addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
-/*         alert("enter"); */
         addTask();
     }
 
 });
 
-/* function checkInput(){
-    if(document.getElementById("txtNewTask").value === "" || document.getElementById("txtResponsible").value === ""){
-        alert("Please enter a Task");
-        return;
-    }else{
-addTask();
-    }
-} */
 function addTask() {
-/*     if(document.getElementById("txtNewTask").value === "" || document.getElementById("txtResponsible").value === ""){
-        alert("Please fill out form");
-        return;
-    }else{ */
     let taskName = document.getElementById("txtNewTask").value;
     let taskResponsible = document.getElementById("txtResponsible").value;
-    let task = { name: taskName, responsible: taskResponsible, isDone: false }
+    if (taskName === "" || taskResponsible === "") {
+        alert("Please fill in both fields");
+        return;
+    } else {
+        let task = { name: taskName, responsible: taskResponsible, isDone: false }
+        tasks.push(task);
+        printTaskList();
+        document.getElementById("txtNewTask").value = "";
+        document.getElementById("txtResponsible").value = "";
+        document.getElementById("txtNewTask").focus();
 
-    tasks.push(task);
-    printTaskList();
-    document.getElementById("txtNewTask").value = "";
-    document.getElementById("txtResponsible").value = "";
+    }}
+    function printTaskList() {
+        document.getElementById("taskList").innerHTML = getHTMLTasks();
+    }
+    function markTask(element) {
+        let index = element.attributes["data-index"].value;
 
-}
-function printTaskList() {
-    document.getElementById("taskList").innerHTML = getHTMLTasks();
-}
-function markTask(element) {
-    let index = element.attributes["data-index"].value;
+        let isChecked = element.checked;
+        tasks[index].isDone = isChecked;
+        printTaskList();
+    }
 
-    let isChecked = element.checked;
-    tasks[index].isDone = isChecked;
-    printTaskList();
-}
+    function deleteTask(element) {
+        let index = element.attributes["data-index"].value;
+        tasks.splice(index, 1);
+        printTaskList();
+        document.getElementById("txtNewTask").focus();
+    }
 
-function deleteTask(element) {
-    let index = element.attributes["data-index"].value;
-    tasks.splice(index, 1);
-    printTaskList();
-}
+    function getHTMLTasks() {
+        let html = "";
+        let index = 0;
 
-function getHTMLTasks() {
-    let html = "";
-    let index = 0;
+        tasks.forEach(element => {
+            let checked = "";
+            if (element.isDone) {
+                checked = "checked";
+            }
+              html += "<li class='list-group-item'><input class='checkbox' onclick='markTask(this)' name='checkbox' data-index='" + index + "' type='checkbox'" + checked + "/>" + "&nbsp" + element.name + "&nbsp" + element.responsible + "&nbsp"  + index + "&nbsp"   + "<div class='right'><button type='button'  onCLick='deleteTask(this)' name='button' data-index='" + index + "'>X</div></div></li>" /* an das HTML anhängen 
 
-    tasks.forEach(element => {
-        let checked = "";
-        if (element.isDone) {
-            checked = "checked";
-        }
-        html += "<li><input  onclick='markTask(this)' name='checkbox' data-index='" + index + "' type='checkbox'" + checked + "/>" + "&nbsp" + element.name + "&nbsp" + element.responsible + "&nbsp" /* + index + "&nbsp"  */+ "<button type='button' onCLick='deleteTask(this)' name='button' data-index='" + index + "'>X</li>" /* an das HTML anhängen */
 
-/*         html += "<li><div class='checkBox'><input  onclick='markTask(this)' name='checkbox' data-index='" + index + "' type='checkbox'" + checked + "/></div><div class='task'>" + element.name + "</div><div class='resposible'>" + element.responsible + "</div><div class='index'>" + index + "</div>" + "<div class='deleteButton'><button type='button' onCLick='deleteTask(this)' name='button' data-index='" + index + "'>X</div></li>" /* an das HTML anhängen */
- 
-        console.log(element);
-        index++;
-    });
-    return html;
-}
+/*             html += "<tr><td><input  onclick='markTask(this)' name='checkbox' data-index='" + index + "' type='checkbox'" + checked + "/></td><td>" + element.name + "</td><td>" + element.responsible + "</td>"  + index + "&nbsp"  + "<td><button type='button' onCLick='deleteTask(this)' name='button' data-index='" + index + "'>X</td></tr>" /* an das HTML anhängen 
+
+/*             <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr> */
+
+
+            /*         html += "<li><div class='checkBox'><input  onclick='markTask(this)' name='checkbox' data-index='" + index + "' type='checkbox'" + checked + "/></div><div class='task'>" + element.name + "</div><div class='resposible'>" + element.responsible + "</div><div class='index'>" + index + "</div>" + "<div class='deleteButton'><button type='button' onCLick='deleteTask(this)' name='button' data-index='" + index + "'>X</div></li>" /* an das HTML anhängen */
+
+            console.log(element);
+            index++;
+        });
+        return html;
+    }
