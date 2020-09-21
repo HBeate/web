2ce8961e1d5730e41f3ae35467656bc2cb483128
HBeate <hauser_b@hotmail.com>
2020-09-21 20:09:42 +0200

    added handlebars template

--- Aufgabe4/Fussballverein/js/fussballverein.js
+++ Aufgabe4/Fussballverein/js/fussballverein.js
@@ -4,7 +4,10 @@ let urlPlayers = "https://api.football-data.org/v2/teams/";
 let urlResults = "https://api.football-data.org/v2/competitions/2002/standings?season=2019";
 let token = '4aaa8f2fcfdb487482bbd4c60c7cf86d';
 
- function loadAllFreeLeagues() {
+function loadAllFreeLeagues() {
+    var source = document.getElementById("freeLeagues-template").innerHTML;
+    var template = Handlebars.compile(source);
+
     fetch(urlLeagues, {
         headers: {
             "x-auth-token": token,
@@ -13,22 +16,23 @@ let token = '4aaa8f2fcfdb487482bbd4c60c7cf86d';
         .then(function (data) {
             let html = "";
 
-
-
-
             data.competitions.forEach(element => {
+                console.log(template(element));
+                html += template(element);
 
-                html += "<tr><td><img src='" + element.area.ensignUrl + "' width='50px' alt='Club Logo'></img></td/><td><a href='#' class='teamLinkPlayers' onclick='loadAllTeams(" + element.id + ")'>" + element.name + "</a></td><td>"+element.area.name+"</td><td>"+element.area.countryCode+"</td></tr>";
-            });
+/*                 html += "<tr><td><img src='" + element.area.ensignUrl + "' width='50px' alt='Club Logo'></img></td/><td><a href='#' class='teamLinkPlayers' onclick='loadAllTeams(" + element.id + ")'>" + element.name + "</a></td><td>" + element.area.name + "</td><td>" + element.area.countryCode + "</td></tr>";
+ */            });
             document.getElementById("leagues").innerHTML = html;
         }).catch(function (err) {
             console.warn('Something went wrong.', err);
         });
-} 
+}
 // ich hole mir die deutschen Teams
 function loadAllTeams(id) {
+    var source = document.getElementById("team-template").innerHTML;
+    var template = Handlebars.compile(source);
+
     let url = urlTeams + id + "/teams?season=2019";
-    console.log(url);
     fetch(url, {
         headers: {
             "x-auth-token": token,
@@ -38,15 +42,19 @@ function loadAllTeams(id) {
         .then(function (data) {
             let html = "";
 
+
             data.teams.forEach(element => {
-                let id = element.id;
-                let team = element.name;
-                html += "<div class='row' data-id='" + element.id + "'><div class='col-sm'><img src='" + element.crestUrl + "'  width='50px' alt='Club Logo'></img></div><div class='col-sm'>" + element.id + "</div><div class='col-sm'><a href='#' class='teamLinkPlayers' onclick='loadPlayersForTeam(" + element.id + ")'>" + element.name + "</a></div><div class='col-sm'>" + element.address + "</div><div class='col-sm'>" + element.email + "</div></div>"
-                // 
+                /*                 console.log(template(element)); */
+
+
+                html += template(element);
+                /*                 html += "<div class='row' data-id='" + element.id + "'><div class='col-sm'><img src='" + element.crestUrl + "'  width='50px' alt='Club Logo'></img></div><div class='col-sm'>" + element.id + "</div><div class='col-sm'><a href='#' class='teamLinkPlayers' onclick='loadPlayersForTeam(" + element.id + ")'>" + element.name + "</a></div><div class='col-sm'>" + element.address + "</div><div class='col-sm'>" + element.email + "</div></div>"
+                 */                // 
 
 
             });
             document.getElementById("teams").innerHTML = html;
+            document.getElementById("teamsJump").innerHTML = data.competition.name;
         }).catch(function (err) {
             // There was an error
             console.warn('Something went wrong.', err);
@@ -54,8 +62,10 @@ function loadAllTeams(id) {
 }
 // ich hole mir die einzelnen Spieler vom jeweiligen Team
 function loadPlayersForTeam(id) {
-let url = urlPlayers + id;
-console.log(url);
+    var source = document.getElementById("playersForTeam-template").innerHTML;
+    var template = Handlebars.compile(source);
+
+    let url = urlPlayers + id;
     fetch(url, {
         headers: {
             "x-auth-token": token
@@ -65,13 +75,15 @@ console.log(url);
             let html = "";
 
             data.squad.forEach(element => {
-                
-                let bdate = new Date(element.dateOfBirth);
-                birthdate = bdate.toLocaleDateString();
+                /*                 let bdate = new Date(element.dateOfBirth);
+                                birthdate = bdate.toLocaleDateString();
+                                 */
+                html += template(element);
 
-/*                 alert('cut:  ' + DOBCut); */
-                html += "<div class='col '><div class='card text-center ' '><img src='../media/player_dummy.jpg' class='card-img-top' alt='Player Picture'><div class='card-body'><h5 class='card-title'>" + element.name + "</h5><p class='card-text'>" + birthdate + "<br>" + element.position + "</p></div></div></div>"
 
+
+                /*                 html += "<div class='col '><div class='card text-center ' '><img src='../media/player_dummy.jpg' class='card-img-top' alt='Player Picture'><div class='card-body'><h5 class='card-title'>" + element.name + "</h5><p class='card-text'>" + birthdate + "<br>" + element.position + "</p></div></div></div>"
+                 */
             });
             document.getElementById("squad").innerHTML = html;
             document.getElementById("teamName").innerHTML = 'Spieler von ' + data.name;
@@ -84,6 +96,9 @@ console.log(url);
 }
 // ich hole mir die Ergebnistabelle
 function loadResults() {
+    var source = document.getElementById("results-template").innerHTML;
+    var template = Handlebars.compile(source);
+
     fetch(urlResults, {
         headers: {
             "x-auth-token": token
@@ -93,9 +108,9 @@ function loadResults() {
             let html = "";
 
             data.standings[0].table.forEach(element => {
-
-                html += "<tr><th scope='row'>" + element.position + "</th><td><img src='" + element.team.crestUrl + "' width='30px'  alt=''></td><td>" + element.team.name + "</td><td>" + element.playedGames + "</td><td>" + element.won + "</td><td>" + element.draw + "</td><td>" + element.lost + "</td><td>" + element.goalsFor + ":" + element.goalsAgainst + "</td><td>" + element.goalDifference + "</td><td>" + element.points + "</td></tr>"
-
+                html += template(element);
+                /*                 html += "<tr><th scope='row'>" + element.position + "</th><td><img src='" + element.team.crestUrl + "' width='30px'  alt=''></td><td>" + element.team.name + "</td><td>" + element.playedGames + "</td><td>" + element.won + "</td><td>" + element.draw + "</td><td>" + element.lost + "</td><td>" + element.goalsFor + ":" + element.goalsAgainst + "</td><td>" + element.goalDifference + "</td><td>" + element.points + "</td></tr>"
+                 */
             });
             document.getElementById("tableResults").innerHTML = html;
         }).catch(function (err) {
